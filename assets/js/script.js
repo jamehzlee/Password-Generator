86a7acaa1f08a87e55ff1e87455c9de88d0dd047
Jamehzz <probableeme@gmail.com>
2022-02-18 09:40:48 -0500

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+// 
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz'.split('');
+console.log(lowerCase);
+
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+console.log(upperCase);
+
+var numerics = '0123456789'.split('');
+console.log(numerics);
+
+var specialChar = '~!@#$%^&*()_+"-=,./\|<>?;:[]{}'.split('');
+console.log(specialChar);
+
+//Generates random characters for any given length 
+for (let i = 0; i < ; i++) {
+  Math.floor(Math.random()*/*array with char, etc.*/0);
+
+}
+var includeLower = window.prompt("Wouuld you like to include lower case letters")
+var includeUpper = window.prompt("Wouuld you like to include upper case letters")
+var includeNum = window.prompt("Wouuld you like to include numbers")
+var includeSpec = window.prompt("Wouuld you like to include special characters")
+
+if (includeLower == true) {
+if (includeUpper == true) {
+if (includeNum == true) {
+if (includeSpec == true) {
+
+}}}}
+
+// Assignment Code
+function generatePassword() {
+  var passwordLength;
+  
+
+  return passwordLength;
+}
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  
+  passwordText.value = password;
+  
+}
+
+var generateBtn = document.querySelector("#generate");
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+}
