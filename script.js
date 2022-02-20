0713c10e2de39d936340bd1e5f71048db62b8ead
Jamehzz <probableeme@gmail.com>
2022-02-20 12:59:06 -0500

    finished charType and random character functions

--- script.js
+++ script.js
@@ -1,41 +1,58 @@
-// 
 var lowerCase = 'abcdefghijklmnopqrstuvwxyz'.split('');
-console.log(lowerCase);
-
 var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-console.log(upperCase);
-
 var numerics = '0123456789'.split('');
-console.log(numerics);
-
 var specialChar = '~!@#$%^&*()_+"-=,./\|<>?;:[]{}'.split('');
-console.log(specialChar);
 
-//Generates random characters for any given length 
-// for (let i = 0; i < ; i++) {
-//   Math.floor(Math.random()*/*array with char, etc.*/0);
+var charPool = []; 
+var passArray = [];
 
-// }
-var includeLower = window.prompt("Would you like to include lower case letters")
-var includeUpper = window.prompt("Would you like to include upper case letters")
-var includeNum = window.prompt("Would you like to include numbers")
-var includeSpec = window.prompt("Would you like to include special characters")
-
-if (includeLower == true) {
-if (includeUpper == true) {
-if (includeNum == true) {
-if (includeSpec == true) {
-
-}}}}
-
-// Assignment Code
-function generatePassword() {
-  var passwordLength;
+function criteria() {
+  const includeLower = confirm("Would you like to include lower case letters");
+  const includeUpper = confirm("Would you like to include upper case letters");
+  const includeNum = confirm("Would you like to include numbers");
+  const includeSpec = confirm("Would you like to include special characters");
   
-
-  return passwordLength;
+  while (passwordLength < 8 || passwordLength >128) {
+    var passwordLength = prompt("Please enter a length between 8-128 characters.");
+  }
+  console.log(criteria);
 }
 
+function pickCharTypes() {
+  if (includeLower == true) {
+    charPool = charPool.concat(lowerCase);
+  }
+  if (includeUpper == true) {
+    charPool = charPool.concat(upperCase);
+  }
+  if (includeNum == true) {
+    charPool = charPool.concat(numerics);    
+  }
+  if (includeSpec == true) {
+    charPool = charPool.concat(specialChar);
+  }
+  console.log(pickCharTypes);
+}
+
+//Generates random characters for any given length 
+function rollChar() {
+  for (let i = 0; i < passwordLength; i++) {
+    var randomNum = Math.floor(Math.random() * charPool.length);
+    var randomChar = charPool[randomNum];
+    passArray.push(randomChar);
+  }
+  console.log(passArray);
+}
+
+function generatePassword() {
+  criteria();
+  pickCharTypes();
+  rollChar();
+  passArray.toString();
+}
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
@@ -43,9 +60,9 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   
   passwordText.value = password;
-  
+  charPool = [];
+  passArray = [];
 }
 
-var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
